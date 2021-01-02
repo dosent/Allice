9f89a295831d918e93cbd3cae416f958cbff0566
Ivan Neshin <ivan.neshin@simbirsoft.com>
2021-01-02 18:52:48 +0400

    Initial commit

---
+++ RN.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/azzzak/alice"
+)
+
+func main() {
+	updates := alice.ListenForWebhook("/hook")
+	go http.ListenAndServe(":3000", nil)
+
+	updates.Loop(func(k alice.Kit) *alice.Response {
+		req, resp := k.Init()
+		if req.IsNewSession() {
+			return resp.Text("привет")
+		}
+		return resp.Text(req.OriginalUtterance())
+	})
+}
